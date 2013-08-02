b77b35138050b402f3634c8e15a8b4a30359ccc0
bacongobbler <me@bacongobbler.com>
2013-08-02 10:16:40 -0700

    Hack: use mysql2:// as the scheme for DATABASE_URL

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+if `gem list`.lines.grep(/^mysql2 \(.*\)/) and ENV['DATABASE_URL']
+  ENV['DATABASE_URL'] = ENV['DATABASE_URL'].sub(/mysql:/, 'mysql2:')
+end
